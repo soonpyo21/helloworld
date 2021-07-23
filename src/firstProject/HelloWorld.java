28429be91ee9085bf70422e0a6d83fc2a006959b
soonpyo21 <tnsvy2004@gmail.com>
2021-07-23 17:31:31 +0900

    first commit

---
+++ src/firstProject/HelloWorld.java
@@ -0,0 +1,14 @@
+package firstProject; // 코드의 그룹을 지을때 사용
+                      //한줄석
+                      /* 여러줄 주석
+                       * 
+                       * 
+                       * 
+                       */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+}
